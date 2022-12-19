69bc6618a6e14d25e32a4069761b495560fc0633
David Ngomane <70548142+Dngomane@users.noreply.github.com>
2022-12-19 15:45:17 +0200

    first committed

--- Main.java
+++ Main.java
@@ -1,58 +1,67 @@
 import java.util.Scanner;
 
-public class Main{
+public class Main {
+
     public static void main(String[] args) {
-       Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+        String username;
+        String password;
 
-        //Display bank name
-       System.out.println("Geeks Bank");
-       System.out.println();
+        System.out.print("Enter your username: ");
+        username = sc.next();
 
-       //display options to choose from
-       System.out.println("1. Check Balance");
-       System.out.println("2. Deposit");
-       System.out.println("3. Send Money");
-       System.out.println("4. Withdraw");
-       System.out.println("5. Cancel/Exit");
-       System.out.println();
+        System.out.print("Enter your pin: ");
+        password = sc.next();
 
-       //accept input from user
+        System.out.println(username + " " + password);
+
+        
+        // Display bank name
+        System.out.println("Geeks Bank");
+        System.out.println();
+
+        // display options to choose from
+        System.out.println("1. Check Balance");
+        System.out.println("2. Deposit");
+        System.out.println("3. Send Money");
+        System.out.println("4. Withdraw");
+        System.out.println("5. Cancel/Exit");
+        System.out.println();
+
+        // accept input from user
         System.out.println("Choose Option: ");
         int option = sc.nextInt();
 
-       //variable to store option entered
-       String choice = " ";
+        // variable to store option entered
+        String choice = " ";
 
-      //test user input
-      switch(option) {
-    
-        case 1: 
-            choice = "Check Balance";
-            break;
-        case 2:
-            choice = "Deposit";
-            break;
-        case 3:
-            choice = "Send Money";
-            break;
-        case 4: 
-            choice = "Withdraw";
-            break;
-        case 5: 
-            choice = "Cancel/Exit";
-            break;
-        case 6: 
-            System.exit(0);
-            break;
-        default:
-            choice = "Invalid Input";
-            break;
-      }
+        // test user input
+        switch (option) {
+
+            case 1:
+                choice = "Check Balance";
+                break;
+            case 2:
+                choice = "Deposit";
+                break;
+            case 3:
+                choice = "Send Money";
+                break;
+            case 4:
+                choice = "Withdraw";
+                break;
+            case 5:
+                choice = "Cancel/Exit";
+                break;
+            case 6:
+                System.exit(0);
+                break;
+            default:
+                choice = "Invalid Input";
+                break;
+        }
 
         // Display the option selected
         System.out.println(choice);
-      
-
-      
     }
 }

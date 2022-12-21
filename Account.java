1216f89554fb1d8a22d02f929f433f224439d99d
David Ngomane <70548142+Dngomane@users.noreply.github.com>
2022-12-21 15:35:21 +0200

    ...

--- Account.java
+++ Account.java
@@ -1,24 +1,24 @@
-public class Account extends User{
+public class Account extends User {
     private String account;
     private double amount;
 
-    Account(String account, Double amount, 
-    String username, String password) {
+    Account(String account, Double amount,
+            String username, String password) {
         super(username, password);
         this.account = account;
         this.amount = amount;
 
     }
 
-    public String getAccount(){
-		return account;
-	}
+    public String getAccount() {
+        return account;
+    }
 
     public double getAmount() {
         return amount;
     }
 
-    public void setAccount(double acc){
+    public void setAccount(double acc) {
         amount = acc;
     }
 }
--- Main.java
+++ Main.java
@@ -1,72 +1,8 @@
 public class Main {
-
-    public static void menu() {
-        // display options to choose from
-        System.out.println("1. Check Balance");
-        System.out.println("2. Deposit");
-        System.out.println("3. Send Money");
-        System.out.println("4. Withdraw");
-        System.out.println("99. Cancel/Exit");
-        System.out.println();
-    }
-
     public static void main(String[] args) {
 
         USSD s = new USSD();
         s.startApplication();
 
-        // Account acc = new Account("12345678", 10000, "Mpumelelo", "1234");
-        // System.out.println(acc.getName());
-
-        // File file = new File(
-        // );
-
-        // Scanner sc = new Scanner(System.in);
-        // String username;
-        // String password;
-        // boolean session = false;
-
-        // // Display bank name
-        // System.out.println("Geeks Bank");
-        // System.out.println();
-
-        // System.out.print("Enter your username: ");
-        // username = sc.next();
-
-        // System.out.print("Enter your pin: ");
-        // password = sc.next();
-
-        // session = true;
-
-        // System.out.println(username + " " + password);
-
-        // int option;
-        // System.out.println("Choose Option: ");
-
-        // while (session) {
-        // menu();
-        // option = sc.nextInt();
-
-        // switch (option) {
-        // case 1:
-        // System.out.println("Check Balance");
-        // break;
-        // case 2:
-        // System.out.println("Deposit");
-        // break;
-        // case 3:
-        // System.out.println("Send Money");
-        // break;
-        // case 4:
-        // System.out.println("Withdraw");
-        // break;
-        // case 99:
-        // session = false;
-        // break;
-        // default:
-        // System.out.println("Invalid Input");
-        // break;
-        // }
-        // }
     }
 }
